9738e3ff7e646be6929f4b66f8c2599b3c90a6b1
Maximiliano Rios <52523737+mrios1999@users.noreply.github.com>
2020-01-17 14:22:44 -0600

    Initial Commit

---
+++ Scripts.js
@@ -0,0 +1,48 @@
+function mobileMenu() {
+  var x = document.getElementById("mobile-dropdown-ul");
+  var y = document.getElementById("mobile-divider");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+    y.style.display = "none";
+  }
+  else {
+    x.style.display = "block";
+    y.style.display = "block";
+  }
+}
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
